fe94b3f83ee72bdc11d89a3e5dffab8a85d78ee0
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-04-03 20:14:45 -0400

    EarthImage Model

---
+++ NASApp/NASApp/EarthImage.swift
@@ -0,0 +1,25 @@
+//
+//  EarthImage.swift
+//  NASApp
+//
+//  Created by Joanna Lingenfelter on 4/3/17.
+//  Copyright © 2017 JoLingenfelter. All rights reserved.
+//
+
+import Foundation
+
+struct EarthImage {
+    
+    var imageURL: URL?
+    
+    init?(json: json) {
+        
+        guard let urlString = json["url"] as? String else {
+            return nil
+        }
+        
+        self.imageURL = URL(string: urlString)
+        
+    }
+    
+}
