ce5ee32526603b68d2f7c8bfd80c28267b9cd0a6
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-04-15 18:09:09 -0400

    Buttons to RoverCell

--- NASApp/RoverCell.swift
+++ NASApp/RoverCell.swift
@@ -13,6 +13,8 @@ class RoverCell: UICollectionViewCell {
     
     static let reuseIdentifier = "RoverCell"
     var roverImageView: UIImageView?
+    var messageButton: UIButton?
+    var saveButton: UIButton?
     
     override func layoutSubviews() {
         
@@ -30,6 +32,20 @@ class RoverCell: UICollectionViewCell {
             roverImageView.rightAnchor.constraint(equalTo: contentView.rightAnchor)
             ])
         
+        let stackView = UIStackView(arrangedSubviews: [saveButton!, messageButton!])
+        contentView.addSubview(stackView)
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 6
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
+            stackView.heightAnchor.constraint(equalToConstant: 40),
+            stackView.widthAnchor.constraint(equalToConstant: 60)
+            ])
+        
     }
     
     func configureCell(withImage image: RoverImage) {
@@ -39,6 +55,20 @@ class RoverCell: UICollectionViewCell {
         if roverImageView == nil {
             roverImageView = UIImageView()
             contentView.addSubview(roverImageView!)
+        }
+        
+        if messageButton == nil {
+            messageButton = UIButton()
+            messageButton!.setImage(UIImage(named: "10"), for: .normal)
+            messageButton!.imageView?.contentMode = .scaleAspectFit
+            contentView.addSubview(messageButton!)
+        }
+        
+        if saveButton == nil {
+            saveButton = UIButton()
+            saveButton!.setImage(UIImage(named: "15"), for: .normal)
+            saveButton!.imageView?.contentMode = .scaleAspectFit
+            contentView.addSubview(saveButton!)
         }
         
         if let imageURL = image.imageURL {
